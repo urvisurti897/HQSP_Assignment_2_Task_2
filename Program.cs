7ecbb7e2e561838eb5f466bb45a6168c4fb1e08d
urvisurti897 <urvisurti897@gmail.com>
2025-02-16 20:32:42 -0500

    Defining ICustomer interface

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 ﻿namespace HQSP_Assignment_2_Task_2
 {
+    interface ICustomer
+    {
+        string Name { get; set; }
+        string BuildingType { get; set; }
+        int Size { get; set; }
+        int LightBulbs { get; set; }
+        int Outlets { get; set; }
+        string CreditCard { get; set; }
+
+        void CreateWiringSchema();
+        void PurchaseParts();
+        void SpecificTask();
+        void DisplayInfo();
+    }
+
     internal class Program
     {
         static void Main(string[] args)
