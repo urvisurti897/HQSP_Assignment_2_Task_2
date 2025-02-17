03d064678c01847f91a13a58a5ad51afec86a8df
urvisurti897 <urvisurti897@gmail.com>
2025-02-16 20:31:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HQSP_Assignment_2_Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
